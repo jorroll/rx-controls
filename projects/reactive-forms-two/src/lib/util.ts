69ec1edc54bcd6e2cae66abca2f7d3209ed3c759
John Carroll <john.carroll.p@gmail.com>
2020-12-09 10:07:01 +0000

    Switch from tiny-isequal to fast-deep-equal

--- projects/reactive-forms-two/src/lib/util.ts
+++ projects/reactive-forms-two/src/lib/util.ts
@@ -1,1 +1,6 @@
-export { isEqual } from '@ngard/tiny-isequal';
+import * as _isEqual from 'fast-deep-equal/es6';
+
+export const isEqual: {
+  <T>(a: T, b: T): boolean;
+  (a: unknown, b: unknown): boolean;
+} = _isEqual as any;
